bd75de7c88dc654dbea1ca244d0b21739a5459f2
zkdn46 <123gusckd123@naver.com>
2024-11-18 15:56:33 +0900

    Update name > classType

--- src/units/Unit.java
+++ src/units/Unit.java
@@ -1,24 +1,26 @@
 package units;
 
 public abstract class Unit {
-	protected String name;
+	protected String classType;
 	protected int hp;
 	protected int maxHp;
 	protected int mp;
+	protected int maxMp;
 	protected int att;
 	protected int def;
 	protected int level;
-	
-	protected Unit(){
-		
+
+	protected Unit() {
+
 	}
 
-	protected Unit(String name, int level, int hp, int mp, int att, int def) {
-		this.name = name;
+	protected Unit(String classType, int level, int hp, int mp, int att, int def) {
+		this.classType = classType;
 		this.level = level;
 		this.hp = hp;
 		this.maxHp = hp;
 		this.mp = mp;
+		this.maxMp = mp;
 		this.att = att;
 		this.def = def;
 	}
