a224f43491d8c1f3a1c93a57a9570117ba5d3bc0
zkdn46 <123gusckd123@naver.com>
2024-11-18 10:35:48 +0900

    Create Project

---
+++ src/textrpg/Main.java
@@ -0,0 +1,8 @@
+package textrpg;
+
+public class Main {
+	public static void main(String[] args) {
+		TextRPG game = getInstance();
+		game.run();
+	}
+}
---
+++ src/textrpg/TextRPG.java
@@ -0,0 +1,5 @@
+package textrpg;
+
+public class TextRPG {
+
+}
