a05cf51f8ddd78260b399fa65bc2354403372401
zkdn46 <123gusckd123@naver.com>
2024-11-18 18:00:56 +0900

    Add townSystem()

--- src/stage/Town.java
+++ src/stage/Town.java
@@ -8,17 +8,22 @@ public class Town {
 	private final int INVEN = 3;
 	private final int SAVE = 4;
 	private final int LOAD = 5;
-	
+
 	private static Town instance = new Town();
 
 	public static Town getInstance() {
 		return instance;
 	}
-	
+
 	private textrpg.Player player = textrpg.Player.getInstance();
 	private Shop shop = Shop.getInstance();
 	private Guild guild = Guild.getInstance();
 
+	public void townRun() {
+		printMenu();
+		townSystem();
+	}
+	
 	public void printMenu() {
 		textrpg.TextRPG.buffer.setLength(0);
 		textrpg.TextRPG.buffer.append("=== 더조은마을 === \n");
@@ -28,8 +33,10 @@ public class Town {
 			textrpg.TextRPG.writer.flush();
 		} catch (IOException e) {
 			e.printStackTrace();
-		}
-
+		}	
+	}
+	
+	private void townSystem() {
 		int sel = textrpg.TextRPG.input("메뉴 선택: ");
 
 		if (sel == GUILD) {
@@ -39,10 +46,10 @@ public class Town {
 		} else if (sel == INVEN) {
 			player.inventory();
 		} else if (sel == SAVE) {
-			
+
 		} else if (sel == LOAD) {
 
 		}
 	}
-	
+
 }
