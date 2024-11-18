8316edc7229cc587d7c16d6c2a3e1a715b624ac3
zkdn46 <123gusckd123@naver.com>
2024-11-18 12:37:39 +0900

    Add Unit()

--- src/units/Unit.java
+++ src/units/Unit.java
@@ -1,5 +1,25 @@
 package units;
 
-public class Unit {
+public abstract class Unit {
+	protected String name;
+	protected int hp;
+	protected int maxHp;
+	protected int mp;
+	protected int att;
+	protected int def;
+	protected int level;
+	
+	protected Unit(){
+		
+	}
 
+	protected Unit(String name, int level, int hp, int mp, int att, int def) {
+		this.name = name;
+		this.level = level;
+		this.hp = hp;
+		this.maxHp = hp;
+		this.mp = mp;
+		this.att = att;
+		this.def = def;
+	}
 }
