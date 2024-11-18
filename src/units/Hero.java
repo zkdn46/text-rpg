7b11ec58a314edc802e874d93ca4cd77b14f0451
zkdn46 <123gusckd123@naver.com>
2024-11-18 15:56:00 +0900

    Add printStatus(), printEquipItem()

--- src/units/Hero.java
+++ src/units/Hero.java
@@ -1,5 +1,109 @@
 package units;
 
-public class Hero {
+import java.io.IOException;
+
+import textrpg.TextRPG;
+
+public class Hero extends Unit {
+	protected int exp;
+	protected boolean isParty;
+	protected textrpg.Item weapon;
+	protected textrpg.Item armor;
+	protected textrpg.Item ring;
+
+	public textrpg.Item getWeapon() {
+		return weapon;
+	}
+
+	public void setWeapon(textrpg.Item weapon) {
+		this.weapon = weapon;
+	}
+
+	public void setArmor(textrpg.Item armor) {
+		this.armor = armor;
+	}
+
+	public void setRing(textrpg.Item ring) {
+		this.ring = ring;
+	}
+
+	public textrpg.Item getArmor() {
+		return armor;
+	}
+
+	public textrpg.Item getRing() {
+		return ring;
+	}
+
+	public Hero(String classType, int level, int hp, int mp, int att, int def) {
+		super(classType, level, hp, mp, att, def);
+	}
+
+	protected Hero() {
+
+	}
+
+	public void printStatus() {
+		TextRPG.buffer.setLength(0);
+		TextRPG.buffer.append(" [직업: " + classType + "] ");
+		TextRPG.buffer.append("[Lv." + level + "] ");
+		TextRPG.buffer.append("\n");
+		TextRPG.buffer.append("[HP: " + hp + " / " + maxHp + "] ");
+		if (ring != null) {
+			TextRPG.buffer.append("[MP: " + mp + " + " + ring.getPower());
+		} else {
+			TextRPG.buffer.append("[MP: " + mp);
+		}
+		if (ring != null) {
+			TextRPG.buffer.append(" / " + maxMp + " + " + ring.getPower() + "] ");
+		} else {
+			TextRPG.buffer.append(" / " + maxMp + "] ");
+		}
+		if (weapon != null) {
+			TextRPG.buffer.append("[ATT: " + att + " + " + weapon.getPower() + "] ");
+		} else {
+			TextRPG.buffer.append("[ATT: " + att + "] ");
+		}
+		if (armor != null) {
+			TextRPG.buffer.append("[DEF: " + def + " + " + armor.getPower() + "] ");
+		} else {
+			TextRPG.buffer.append("[DEF: " + def + "] ");
+		}
+		TextRPG.buffer.append("\n");
+		try {
+			TextRPG.writer.append(TextRPG.buffer);
+			TextRPG.writer.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public void printEquitedItem() {
+		TextRPG.buffer.setLength(0);
+		if (weapon == null) {
+			TextRPG.buffer.append("[무기: 없음] ");
+		} else {
+			TextRPG.buffer.append("[무기: " + weapon.getName() + "] ");
+		}
+		if (armor == null) {
+			TextRPG.buffer.append("[방어구: 없음] ");
+		} else {
+			TextRPG.buffer.append("[방어구: " + armor.getName() + "] ");
+		}
+		if (ring == null) {
+			TextRPG.buffer.append("[반지: 없음] ");
+		} else {
+			TextRPG.buffer.append("[반지: " + ring.getName() + "] ");
+		}
+		TextRPG.buffer.append("\n");
+		try {
+			TextRPG.writer.append(TextRPG.buffer);
+			TextRPG.writer.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
 
 }
