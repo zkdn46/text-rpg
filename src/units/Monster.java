f42f1edf062a3eba67277e610b0d7a5ff9d8c511
zkdn46 <123gusckd123@naver.com>
2024-11-19 17:58:13 +0900

    Update

--- src/units/Monster.java
+++ src/units/Monster.java
@@ -1,5 +1,9 @@
 package units;
 
-public class Monster {
+public abstract class Monster extends Unit {
 
+	protected Monster(String classType, int level, int hp, int mp, int att, int def) {
+		super(classType, level, hp + (25 * level), mp + (5 * level), att + (2 * level), def + (2 * level));
+	}
+	
 }
