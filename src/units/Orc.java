c63d58e2b3156aaf2b2a3ccb9186ebb96e07e9e3
zkdn46 <123gusckd123@naver.com>
2024-11-19 18:26:49 +0900

    Add attack()

--- src/units/Orc.java
+++ src/units/Orc.java
@@ -1,14 +1,56 @@
 package units;
 
+import java.io.IOException;
+import java.util.Random;
+
+import textrpg.TextRPG;
+
 public class Orc extends Monster {
 
+	private Random ran = new Random();
+
 	public Orc(String classType, int level, int hp, int mp, int att, int def) {
-		super(classType, level, hp+30, mp, att+5, def);
-		
+		super(classType, level, hp + 30, mp, att + 5, def);
+
 	}
-	
-	public void attack() {
-		
+
+	public void attack(Unit enenmy) {
+		Hero target = (Hero) enenmy;
+		int skill = ran.nextInt(3);
+		int damage = 0;
+
+		if (mp >= 20 && skill == 1) {
+			mp -= 20;
+
+			damage = att * 2 - target.def;
+
+			TextRPG.buffer.setLength(0);
+			TextRPG.buffer.append("쎄게 치기!\n");
+			try {
+				TextRPG.writer.append(TextRPG.buffer);
+				TextRPG.writer.flush();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		} else {
+			damage = att - target.def;
+		}
+
+		if (damage < 1) {
+			System.err.println("MISS!");
+			return;
+		}
+		target.hp -= damage;
+
+		TextRPG.buffer.setLength(0);
+		TextRPG.buffer.append(target.classType + "에게 피해 " + damage + " 입힘\n");
+		try {
+			TextRPG.writer.append(TextRPG.buffer);
+			TextRPG.writer.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 	}
 
 }
