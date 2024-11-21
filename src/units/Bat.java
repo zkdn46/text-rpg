656863785a4e8116f90829ecd6a6d9c4ad133e6b
zkdn46 <123gusckd123@naver.com>
2024-11-21 18:24:18 +0900

    Fix attack()

--- src/units/Bat.java
+++ src/units/Bat.java
@@ -4,10 +4,10 @@ import java.io.IOException;
 
 import textrpg.TextRPG;
 
-public class Bat extends Monster{
+public class Bat extends Monster {
 
 	public Bat(String classType, int level, int hp, int mp, int att, int def) {
-		super(classType, level, hp, mp, att+5, def);
+		super(classType, level, hp, mp, att + 5, def);
 	}
 
 	public void attack(Unit enenmy) {
@@ -15,13 +15,25 @@ public class Bat extends Monster{
 		int skill = Orc.ran.nextInt(3);
 		int damage = 0;
 
+		damage = att - target.def;
+
+		if (damage < 1) {
+			System.err.println("MISS!");
+			return;
+		}
+
 		if (mp >= 20 && skill == 1) {
 			mp -= 20;
 
-			damage = att - target.def;
+			target.hp -= damage;
+			hp += damage / 2;
+			if (hp > maxHp) {
+				hp = maxHp;
+			}
 
 			TextRPG.buffer.setLength(0);
 			TextRPG.buffer.append("흡혈!\n");
+			TextRPG.buffer.append("박쥐 HP " + damage / 2 + " 회복\n");
 			try {
 				TextRPG.writer.append(TextRPG.buffer);
 				TextRPG.writer.flush();
@@ -29,19 +41,11 @@ public class Bat extends Monster{
 				e.printStackTrace();
 			}
 		} else {
-			damage = att - target.def;
+			target.hp -= damage;
 		}
-
-		if (damage < 1) {
-			System.err.println("MISS!");
-			return;
-		}
-		target.hp -= damage;
-		hp+=damage/2;
 
 		TextRPG.buffer.setLength(0);
 		TextRPG.buffer.append(target.classType + "에게 피해 " + damage + " 입힘\n");
-		TextRPG.buffer.append("박쥐 HP " + damage/2 + " 회복\n");
 		try {
 			TextRPG.writer.append(TextRPG.buffer);
 			TextRPG.writer.flush();
