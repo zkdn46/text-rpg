fa5a4e61e599d6443dcc19ec793808a3b71b3117
zkdn46 <123gusckd123@naver.com>
2024-11-21 18:24:49 +0900

    Fix attack()

--- src/units/Slime.java
+++ src/units/Slime.java
@@ -1,10 +1,55 @@
 package units;
 
+import java.io.IOException;
+
+import textrpg.TextRPG;
+
 public class Slime extends Monster {
 
 	public Slime(String classType, int level, int hp, int mp, int att, int def) {
-		super(classType, level, hp+20, mp, att, def+5);
+		super(classType, level, hp+20, mp, att, def+3);
 		
+	}
+
+
+	public void attack(Unit enenmy) {
+		Hero target = (Hero) enenmy;
+		int skill = Orc.ran.nextInt(3);
+		int damage = 0;
+		
+		damage = att - target.def;
+		
+		if (damage < 1) {
+			System.err.println("MISS!");
+			return;
+		}
+		
+		target.hp -= damage;
+
+		if (mp >= 20 && skill == 1) {
+			mp -= 20;
+
+			target.mp -= damage/2;
+
+			TextRPG.buffer.setLength(0);
+			TextRPG.buffer.append("마나공격!\n");
+			TextRPG.buffer.append("마나 피해 " + damage/2 + " 입힘\n");
+			try {
+				TextRPG.writer.append(TextRPG.buffer);
+				TextRPG.writer.flush();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		TextRPG.buffer.setLength(0);
+		TextRPG.buffer.append(target.classType + "에게 피해 " + damage+" 입힘\n");
+		try {
+			TextRPG.writer.append(TextRPG.buffer);
+			TextRPG.writer.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
